89f86674a59e133b1a8ffccc8f791ec62c2d7dd0
Julian <juliankimmings@gmail.com>
2020-03-08 10:09:28 +0000

    init from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
+import SearchBox from './components/SearchBox/SearchBox';
+import { BODY_COLOR, HERO_BG_COLOR } from './constants';
 import './App.css';
+
+const MainBody = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  color: ${BODY_COLOR};
+`;
+
+const Hero = styled.div`
+  padding-bottom: 400px;
+  background-color: ${HERO_BG_COLOR};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainBody>
+      <Hero />
+      <SearchBox />
+    </MainBody>
   );
 }
 
